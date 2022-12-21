7c31c07c09385fa055a932377c748972e096ceeb
ClausR1990 <claus.reinhold@gmail.com>
2022-12-21 14:30:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# React | Vite - User Rolodex
+
+Just a random react app built with Vite. Using "dummy" api to fetch a list of users with react hooks.
+
+## API Reference
+
+#### Get all users
+
+```http
+  GET https://dummyjson.com/users
+```
+
+## Installation
+
+Install my-project with npm
+
+```bash
+  npm install
+  npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react'
+
+interface User {
+    id: number
+    firstName: string
+    lastName: string
+    email: string
+    age: number
+    image: string
+}
+
+const App = () => {
+    const [users, setUsers] = useState<User[] | null>(null)
+    const [searchTerm, setSearchTerm] = useState<string>('')
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    `https://dummyjson.com/users/search?q=${searchTerm}&select=firstName,lastName,age,email,image`
+                )
+                const data = await response.json()
+                if (!data) return
+                setUsers(data.users)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        fetchData()
+
+        return () => {}
+    }, [searchTerm])
+
+    const updateSearchTerm = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value.length < 3) {
+            setSearchTerm('')
+        }
+
+        setSearchTerm(e.target.value)
+    }
+
+    return (
+        <main className='bg-zinc-900 min-h-screen w-full'>
+            <section className='py-24'>
+                <div className='container text-center'>
+                    <input
+                        className='p-3 px-5 outline outline-gray-500 appearance-none rounded-md w-full'
+                        type='text'
+                        name='searchField'
+                        onChange={(e) => updateSearchTerm(e)}
+                        placeholder='Search...'
+                    />
+                    {users && users?.length > 0 ? (
+                        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mt-12'>
+                            {users.slice(0, 8).map((user) => (
+                                <div
+                                    className='p-6 bg-slate-600 rounded-lg border border-slate-200'
+                                    key={user.id}
+                                >
+                                    <div>
+                                        <img
+                                            className='w-full aspect-square object-contain mb-8'
+                                            src={user.image}
+                                        />
+                                    </div>
+                                    <h2 className='text-white font-bold'>{`${user.firstName} ${user.lastName} | ${user.age}`}</h2>
+                                    <a>{user.email}</a>
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <p className='text-white'>No user found 😥</p>
+                    )}
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default App
